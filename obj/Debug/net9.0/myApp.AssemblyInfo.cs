6b12e3f530113c8532e7f03c6b068850617e5b26
vortep.7 <avpetrov@derzhava.online>
2024-11-30 12:47:59 +0300

    [add] README file

--- obj/Debug/net9.0/myApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/myApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+892903c298c1b5e9680b746068739468ee77ab2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c72a1ab032572e3ec4086efcd34f4510f949cf0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("myApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
